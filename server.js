76579510e92bec3ae3f5d0edcaa2478c9dace23f
CalebBX <cbrocomp@gmail.com>
2017-10-18 20:57:53 -0700

    Added project page

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/about', function(req, res){
     });
 });
 
+app.get('/projects', function(req, res){
+    res.render('projects.hbs', {
+       pageTitle: 'Project Page',   
+    });
+});
+
 app.get('/bad', function(req, res){
    res.send({
        errorMessage: 'Unable to handle request.'
